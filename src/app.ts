8f804bd076fd5feaa8770195502c6620d1cf534d
SamuelLlanes <samuelllanes16@gmail.com>
2026-07-03 11:45:06 -0600

    Revert "Show y Delete"
    This reverts commit 012e0709d1be8e8da30e43c079f1d7d56493b2d0.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import postsRouter from "./posts.router";
 
 const app = express();
 
@@ -8,7 +7,5 @@ app.use(express.json());
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
-
-app.use("/posts", postsRouter);
 
 export default app;
